0e536d1a7911422574e89eed1c80165e7d75d922
Abhishek Sharma <abhishekmeonly.sharma@gmail.com>
2018-10-27 17:01:23 +0530

    heap implementation using list

---
+++ Python_DS/Trees/binary_search_tree.py
@@ -0,0 +1,81 @@
+'''
+Python implementation of BST. 
+'''
+
+
+class BinarySearchTree(object):
+    def __init__(self, value):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+    def insert(self, new_val):
+        if  new_val> self.value:
+            #Go to right_child subtree
+            if self.right_child:
+                self.right_child.insert(new_val)
+            else:
+                self.right_child = BinarySearchTree(new_val)
+        else:
+            #Go to left subtree
+            if self.left_child:
+                self.left_child.insert(new_val)
+            else:
+                self.left_child= BinarySearchTree(new_val)
+
+    def search(self, find_val):
+        curr=self
+        if curr.value< find_val  and curr.right_child:
+            return curr.right_child.search(find_val)
+        elif curr.value>find_val and curr.left_child:
+            return curr.left_child.search(find_val)
+        return curr.value== find_val
+
+    def delete(self,del_val):
+        '''
+        Little tricky. Cqn divide it into 3 cases.If node to be deleted has :
+        a. 2 children
+        b. 1 children
+        c. No children (Straight Forward)
+        '''
+    def print_tree(self):
+        print "INORDER", self.inorder_print(self, "")[:-1]
+
+
+    def inorder_print(self,start,traversal):
+        '''
+        left_child-- root---right_child
+        :param start:
+        :param traversal:
+        :return:
+        '''
+        if start:
+            traversal= self.inorder_print(start.left_child,traversal)
+            traversal+= (str(start.value)+"-")
+            traversal= self.inorder_print(start.right_child,traversal)
+        return  traversal
+
+
+if __name__ == '__main__':
+    # Set up tree
+    tree = BinarySearchTree(4)
+    # Insert elements
+    tree.insert(2)
+    tree.insert(1)
+    tree.insert(3)
+    tree.insert(5)
+
+    '''
+                                        4
+                                       / \
+                                      2   5
+                                     / \
+                                    1   3
+    '''
+
+    tree.print_tree()
+
+    # Check search
+    # Should be True
+    for i in xrange(0,10):
+        print i,tree.search(i)
---
+++ Python_DS/Trees/heap_python_implementation.py
@@ -0,0 +1,66 @@
+'''
+python 2,7 implementation of heap using arrays from interactive python tut
+http://interactivepython.org/courselib/static/pythonds/Trees/BinaryHeapImplementation.html
+
+'''
+
+
+class BinHeap:
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+
+    def percUp(self, i):
+        while i / 2 > 0:
+            if self.heapList[i] < self.heapList[i / 2]:
+                self.heapList[i/2], self.heapList[i]= self.heapList[i],self.heapList[i/2]
+            i /= 2
+
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize += 1
+        self.percUp(self.currentSize)
+
+    def percDown(self, i):
+        while (i * 2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                self.heapList[mc], self.heapList[i] = self.heapList[i], self.heapList[mc]
+            i = mc
+
+    def minChild(self, i):
+        if i * 2 + 1 > self.currentSize:
+            return i * 2
+        else:
+            if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def delMin(self):
+        retval = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize -= 1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
+
+    def buildHeap(self, alist):
+        i = len(alist) / 2
+        self.currentSize = len(alist)
+        self.heapList = [0] + alist[:]
+        while (i > 0):
+            self.percDown(i)
+            i -= 1
+
+    def buildHeap_using_insert(self, alist):
+        for i in alist:
+            self.insert(i)
+
+
+if __name__ == '__main__':
+    bh = BinHeap()
+    bh.buildHeap([9, 5, 6, 2, 3])
+
+    while bh.currentSize > 0:
+        print(bh.delMin())
