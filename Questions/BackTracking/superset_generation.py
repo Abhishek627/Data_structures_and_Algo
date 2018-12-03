8cbd5f3284b4777ee84e19bee338cc55781964a2
Abhishek Sharma <abhishekmeonly.sharma@gmail.com>
2018-12-04 03:30:20 +0530

    Bit manipulation to generate power set

--- Questions/BackTracking/superset_generation.py
+++ Questions/BackTracking/superset_generation.py
@@ -20,8 +20,25 @@ def superset_generation(input_list,start,curr_list,answer):
         superset_generation(input_list,i+1,curr_list+[input_list[i]],answer)
 
 
+def superset_bit(input_list):
+    # use bit manipulation to generate power set
+    num_elm= len(input_list)
+    power_set_len= 2** num_elm
+    answer= [[] for i in xrange(power_set_len)]
+    for i in xrange(num_elm):
+        for j in xrange(power_set_len):
+            # print j>>i
+            if (j >> i) & 1 :
+                answer[j].append(input_list[i])
+    return answer
+
+
+
+
 if __name__ == '__main__':
     input_list= [1,2,3]
     output=[]
     superset_generation(input_list,0,[],output)
     print output
+
+    print superset_bit(input_list)
