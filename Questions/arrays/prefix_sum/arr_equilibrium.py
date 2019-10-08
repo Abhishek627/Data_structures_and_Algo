38408b7d24c0bd4231cb65e91ae3503d1e6a6f30
shabhishek <shabhishek@juniper.net>
2019-10-08 23:41:25 +0530

    leetcode contest and backtracking questions

---
+++ Questions/arrays/prefix_sum/arr_equilibrium.py
@@ -0,0 +1,19 @@
+'''
+Equilibrium point is index in array for which left and right subarray sum is equal
+'''
+
+
+def equilibrium_point(arr):
+    arr_sum = sum(arr)
+    left_sum = 0
+    for idx, val in enumerate(arr):
+        arr_sum -= val
+        if left_sum == arr_sum:
+            print idx
+        left_sum += val
+    return -1
+
+
+if __name__ == '__main__':
+    arr = [-7, 1, 5, 2, -4, 3, 0]
+    equilibrium_point(arr)
